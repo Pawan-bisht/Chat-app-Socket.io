2a468de1d7d35be1ea4693d0d6586115a455c832
Pawan bisht <pawanbisht83@gmail.com>
2020-06-11 22:21:15 +0530

    The acknowledge and bad-words filtering

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,6 +6,7 @@ socket.on("countUpdated", (count, str) => {
 
 socket.on("message", (message) => {
     console.log(message);
+
 })
 
 //Accepts the two argument first is the name of event 
@@ -17,16 +18,24 @@ document.querySelector("#increment").addEventListener("click", () => {
     socket.emit("clientMessage", "Message from  client 2");
 })
 
+var color = "green";
 let inputMsg = document.getElementById("send-message");
 let form = document.querySelector("#form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    socket.emit("sendMessage", inputMsg.value);
-
+    let msg = event.target.message.value;
+    console.log(msg);
+    //socket.emit("sendMessage", inputMsg.value);
+    socket.emit("sendMessage", msg, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log("Message delivered");
+    });
 })
 
 socket.on("serverMessage", (serverMsg) => {
-    console.log(serverMsg);
+    // console.log(serverMsg);
     let para = document.querySelector("#all-message")
     var node = document.createElement("LI"); // Create a <li> node
     var textnode = document.createTextNode(serverMsg);
@@ -34,4 +43,21 @@ socket.on("serverMessage", (serverMsg) => {
     para.appendChild(node);
     inputMsg.value = ""
 
+})
+
+document.querySelector("#send-location").addEventListener("click", () => {
+    if (!navigator.geolocation) {
+        return alert("Geolocation is not supported by your browser");
+    } else {
+        navigator.geolocation.getCurrentPosition((position) => {
+            // console.log(position.coords.latitude);
+            // console.log(position.coords.longitude);
+            socket.emit("sendLocation", {
+                long: position.coords.longitude,
+                lati: position.coords.latitude
+            }, () => {
+                console.log("Location shared!")
+            });
+        })
+    }
 })
--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 const app = express();
+const Filter = require("bad-words");
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 const io = socketio(server);
-
 
 let publicPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicPath))
 
 let count = 0;
+//connection is the in-built function for socket whenver a new client is connected
 io.on("connection", (socket) => { //listening to a given event to occur
-
     console.log("We are connected now");
     //To send an event to client we use socket.emit.
     //Event is made up of name of event and the event parameters  
@@ -27,10 +27,27 @@ io.on("connection", (socket) => { //listening to a given event to occur
     //     io.emit("countUpdated", count); //this will emit an event to every single connection/client
     // })
 
-    socket.emit("message", "Welcome!")
-    socket.on("sendMessage", (clientMessag) => {
-        console.log(clientMessag)
-        io.emit("serverMessage", clientMessag);
+    socket.emit("message", "Welcome!");
+    socket.broadcast.emit("message", "A new user has joined");
+    let filter = new Filter();
+    socket.on("sendMessage", (clientMessage, callback) => {
+        console.log(clientMessage);
+
+        if (filter.isProfane(clientMessage)) {
+            return callback("Profanity is not allowed!!!");
+        }
+
+        io.emit("serverMessage", clientMessage);
+        callback();
+    })
+
+    socket.on("sendLocation", (coords, callback) => {
+        socket.broadcast.emit("message", `https://google.com/maps?q=${coords.lati},${coords.long}`);
+        callback();
+    })
+
+    socket.on("disconnect", () => {
+        io.emit("message", "A user has left");
     })
 })
 
